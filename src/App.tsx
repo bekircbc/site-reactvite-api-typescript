4baa0f01b5c62395ef3cd58a152750f385693d58
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-30 15:49:21 +0200

    changed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import "./App.scss";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.scss';
+import { IBook } from './data/interfaces';
+import { getBooks } from './data/bookModel';
+
+const url = 'https://gutendex.com/books/?search=paris';
 
 function App() {
-  return <div className="App">Hallo</div>;
+	const [books, setBooks] = useState<IBook[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const rawBooks = (await axios.get(url)).data.results;
+			const _books: IBook[] = getBooks(rawBooks);
+			setBooks(_books);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<p>There are {books.length} books.</p>
+			<ul>
+				{books.map((book, index) => {
+					return (
+						<li key={index}>
+							{book.title} by {book.author}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
